cb347616d70ab03a7ace06a7a5fc644461317c2e
duthli <hung1996latoi2@gmail.com>
2016-09-13 16:13:19 +0700

    Initial Commit

---
+++ End4Kid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  End4Kid
+//
+//  Created by do duy hung on 13/09/2016.
+//  Copyright © 2016 do duy hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
